79a61723b3737e9f53932c389bc000026afdc0d8
1997prachi <prachi.princy97@gmail.com>
2020-06-21 21:39:19 +0530

    update

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,25 +7,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  //getter
-  void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  @override
+  //if we want something to happen the moment that our stateful widget is created and
+  // add into the tree, then we're going to put our code inside initState.
+  //If we want something to happen every single time
+  //only created at once
+  void initState()  {
+    super.initState();
+    getlocation();
   }
+
+  //getter
+  void getlocation() async {
+    Location mylocation = Location();
+    await mylocation.getCurrentLocation();
+    print(mylocation.latitude);
+    print(mylocation.longitude);
+  }
+
+  // if we want something to happen when our stateful widget gets destroyed, then we would put
+  // the code inside the deactivate method.
+  // @override
+  // void deactivate() {
+  //   super.deactivate();
+  //   print('deactivated');
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }

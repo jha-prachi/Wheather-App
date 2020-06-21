3bae35bf62592dde2e5481fe5884e691d89d3a9a
1997prachi <prachi.princy97@gmail.com>
2020-06-21 22:28:05 +0530

    ...

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,7 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   // add into the tree, then we're going to put our code inside initState.
   //If we want something to happen every single time
   //only created at once
-  void initState()  {
+  void initState() {
     super.initState();
     getlocation();
   }
@@ -25,6 +26,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(mylocation.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+
+    //response.statusCode
+    //(body)same data milega jo mujhe browser par mila tha
+  }
+
   // if we want something to happen when our stateful widget gets destroyed, then we would put
   // the code inside the deactivate method.
   // @override
@@ -35,6 +51,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }

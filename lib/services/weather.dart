4e0be6828d1309dc792c6e942aa94770ec7ded93
1997prachi <prachi.princy97@gmail.com>
2020-07-10 23:36:41 +0530

    Updating weather

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,24 @@
+import 'package:clima/services/networking.dart';
+import 'package:clima/services/location.dart';
+
+const apikey = 'a8d75183b85478dff0b4ebd264e804ab';
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+double latitude;
+double longitude;
+
 class WeatherModel {
+  Future<dynamic> getlocationweather() async {
+    Location mylocation = Location();
+    await mylocation.getCurrentLocation();
+    latitude = mylocation.latitude;
+    longitude = mylocation.longitude;
+    NetworkHelper networkHelper = NetworkHelper(
+        '$url?lat=$latitude&lon=$longitude&appid=$apikey&units=metric');
+
+    var wheatherdata = await networkHelper.getData();
+    return wheatherdata;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
